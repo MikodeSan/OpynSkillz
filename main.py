308922be70598499ecb5785a079e7febb30cf1b6
tousmi01 <mickael.toussaint@somfy.com>
2020-10-15 14:35:15 +0200

    search channel

--- main.py
+++ main.py
@@ -55,56 +55,66 @@ YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
 def youtube_search(query):
-  youtube = build(
-      YOUTUBE_API_SERVICE_NAME,
-      YOUTUBE_API_VERSION,
-      developerKey=DEVELOPER_KEY,
-      )
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=query,
-    part='id,snippet',
-    maxResults=21
-  ).execute()
+    youtube = build(
+        YOUTUBE_API_SERVICE_NAME,
+        YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY,
+        )
 
-  videos = []
-  channels = []
-  playlists = []
+    # channels.list(part="id", mine=true)
+    # channels.list(part="id", forUsername="username")
 
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get('items', []):
-    if search_result['id']['kind'] == 'youtube#video':
-      videos.append('%s (%s)' % (search_result['snippet']['title'],
-                                 search_result['id']['videoId']))
-    elif search_result['id']['kind'] == 'youtube#channel':
-      channels.append('%s (%s)' % (search_result['snippet']['title'],
-                                   search_result['id']['channelId']))
-    elif search_result['id']['kind'] == 'youtube#playlist':
-      playlists.append('%s (%s)' % (search_result['snippet']['title'],
-                                    search_result['id']['playlistId']))
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+        q=query,
+        part='id,snippet',
+        type='channel',
+        maxResults=32,
+    ).execute()
 
-  print('Videos:\n   -', '\n   -'.join(videos), '\n') 
-  print('Channels:\n', '\n'.join(channels), '\n')
-  print('Playlists:\n', '\n'.join(playlists), '\n')
+    videos = []
+    channels = []
+    playlists = []
+
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+
+        if search_result['id']['kind'] == 'youtube#video':
+            videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                    search_result['id']['videoId']))
+        
+        elif search_result['id']['kind'] == 'youtube#channel':
+            channels.append('%s (%s)' % (search_result['snippet']['title'],
+                                    search_result['id']['channelId']))
+        
+        elif search_result['id']['kind'] == 'youtube#playlist':
+            playlists.append('%s (%s)' % (search_result['snippet']['title'],
+                                        search_result['id']['playlistId']))
+
+    print('Videos:\n   -', '\n   -'.join(videos), '\n') 
+    print('Channels:\n', '\n'.join(channels), '\n')
+    print('Playlists:\n', '\n'.join(playlists), '\n')
 
 
-# if __name__ == '__main__':
-#   parser = argparse.ArgumentParser()
-#   parser.add_argument('--q', help='Search term', default='Google')
-#   parser.add_argument('--max-results', help='Max results', default=25)
-#   args = parser.parse_args()
+    # if __name__ == '__main__':
+    #   parser = argparse.ArgumentParser()
+    #   parser.add_argument('--q', help='Search term', default='Google')
+    #   parser.add_argument('--max-results', help='Max results', default=25)
+    #   args = parser.parse_args()
 
-#   try:
-#     youtube_search(args)
-#   except HttpError, e:
-#     print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
+    #   try:
+    #     youtube_search(args)
+    #   except HttpError, e:
+    #     print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
 
 
 if __name__ == '__main__':
 
     youtube_search("apprendre la photo")
-    main()
+    youtube_search("marketing mania")
+    youtube_search("my new life")
+    # main()
 
