28ed252300c7d8896c8c56d2976b12d8c01b25af
tousmi01 <mickael.toussaint@somfy.com>
2020-10-20 17:14:48 +0200

    add path app and index view

--- path/views.py
+++ path/views.py
@@ -4,4 +4,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    # if request.user.is_authenticated:
+
+    context = {}
+    context['Message'] = 'Hello Moto'
+
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    return render(request, 'path/index.html', context)        # locals()
+
+
+
+
+
