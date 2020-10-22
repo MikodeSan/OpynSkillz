b352046ed7a4afafabac35015807b7877e1de9e2
tousmi01 <mickael.toussaint@somfy.com>
2020-10-23 01:10:15 +0200

    path creation page

--- path/urls.py
+++ path/urls.py
@@ -6,4 +6,10 @@ app_name = 'path'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('channel/', views.channel, name='channel'),
+    path('channel/path/new', views.path_new, name='path_new'),
+    path('channel/path/create', views.create_path, name='path_create'),
+
+
+    
 ]
--- path/views.py
+++ path/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 
 def index(request):
 
     # if request.user.is_authenticated:
-
     context = {}
     context['Message'] = 'Hello Moto'
 
@@ -15,6 +14,35 @@ def index(request):
     return render(request, 'path/index.html', context)        # locals()
 
 
+def channel(request):
+    
+    context = {}
+    context['Message'] = 'Hello Moto'
 
+    return render(request, 'path/channel.html', context)
+
+
+def path_new(request):
+
+    context = {}
+    context['Message'] = 'Hello Moto'
+
+    return render(request, 'path/path_new.html', context)
+
+
+def create_path(request):
+
+    context = {}
+    context['Message'] = 'Hello Moto'
+
+    print('Path label:', request)
+
+    if request.method == 'POST':
+    
+        label = request.POST.get('path_label')
+        description = request.POST.get('description')
+        print('Path label: {}, Description: {}'.format(label, description))
+
+    return redirect('path:channel')
 
 
